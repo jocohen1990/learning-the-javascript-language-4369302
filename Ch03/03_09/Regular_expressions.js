b19aa47153204122f167440a4a09e5c9a29cb190
jocohen1990 <104044442+jocohen1990@users.noreply.github.com>
2024-12-27 00:02:13 +0000

    #adding comments

--- Ch03/03_09/Regular_expressions.js
+++ Ch03/03_09/Regular_expressions.js
@@ -3,7 +3,7 @@ var string2 = "This is the shortest string ever.";
 var string3 = "Is this the thing called Mount Everest?";
 var string4 = "This is the Sherman on the Mount.";
 
-var regex = /this/;
+var regex = /this/; //this means to make the word this case insensitive
 
 regex.test(string1);
 regex.test(string2);
@@ -12,13 +12,13 @@ regex.test(string4);
 
 regex = /this/i;
 
-regex = /^this/i;
+regex = /^this/i; // match against the beginning of the string
 
-regex = /this$/i;
+regex = /this$/i; // match against the ending of the string
 
-regex = /ever.$/i;
+regex = /ever.$/i; // see if this word appears at the end of the string
 
-regex = /ever\.$/i;
+regex = /ever\.$/i; // see if the punctuation mark . appears at the end of the string
 
 // More info:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
